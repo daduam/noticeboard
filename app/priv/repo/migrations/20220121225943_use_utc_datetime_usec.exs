60c465a7d849f507b0ed2b00956143273b31b8dd
daduam <jkampadu002@st.ug.edu.gh>
2022-01-21 23:22:22 +0000

    fix execute queries

--- app/priv/repo/migrations/20220121225943_use_utc_datetime_usec.exs
+++ app/priv/repo/migrations/20220121225943_use_utc_datetime_usec.exs
@@ -4,18 +4,24 @@ defmodule Noticeboard.Repo.Migrations.UseUtcDatetimeUsec do
   def change do
     # users
     Enum.each(["confirmed_at", "inserted_at", "updated_at"], fn column ->
-      execute("""
-      ALTER TABLE users
-      ALTER #{column} TYPE timestamptz USING #{column} AT TIME ZONE 'UTC'
-      , ALTER #{column} SET DEFAULT now();
-      """)
+      execute(
+        """
+        ALTER TABLE users
+        ALTER #{column} TYPE timestamptz USING #{column} AT TIME ZONE 'UTC'
+        , ALTER #{column} SET DEFAULT now();
+        """,
+        ""
+      )
     end)
 
     # users_tokens
-    execute("""
-    ALTER TABLE users_tokens
-    ALTER inserted_at TYPE timestamptz USING inserted_at AT TIME ZONE 'UTC'
-    , ALTER inserted_at SET DEFAULT now();
-    """)
+    execute(
+      """
+      ALTER TABLE users_tokens
+      ALTER inserted_at TYPE timestamptz USING inserted_at AT TIME ZONE 'UTC'
+      , ALTER inserted_at SET DEFAULT now();
+      """,
+      ""
+    )
   end
 end
